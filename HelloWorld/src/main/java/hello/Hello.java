e0fa472dfc6f69e69630eabbe91b0152e1429e40
igor ujiie <igorkazuhico@hotmail.com>
2022-09-17 20:38:48 -0300

    Servelet

---
+++ HelloWorld/src/main/java/hello/Hello.java
@@ -0,0 +1,29 @@
+package hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<HTML><BODY>");
+		out.println("<P>Alo Mundo!</P>");
+		out.println("</BODY></HTML>");
+		out.close(); 
+
+	}
+
+}
