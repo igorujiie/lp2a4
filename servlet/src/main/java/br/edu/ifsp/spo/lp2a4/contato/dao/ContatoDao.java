ddc70f64df2d74947d977cefbef67d98c79dcd65
igor k. ujiie <64502164+igorujiie@users.noreply.github.com>
2022-10-22 21:40:47 -0300

    Add files via upload

---
+++ servlet/src/main/java/br/edu/ifsp/spo/lp2a4/contato/dao/ContatoDao.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.spo.lp2a4.contato.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import br.edu.ifsp.spo.lp2a4.contato.model.Contato;
+
+public class ContatoDao {
+
+	static Map<Long, Contato> bancofake = new HashMap<>();
+
+	public void adiciona(Contato contato) {
+		// TODO Auto-generated method stub
+		
+	}
+}
